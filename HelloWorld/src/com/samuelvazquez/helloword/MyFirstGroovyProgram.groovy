7f2150a4e6e429b4ef9466d22dadce798bb1f371
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-10-23 06:49:35 -0500

    added my first groovy lines

---
+++ HelloWorld/src/com/samuelvazquez/helloword/MyFirstGroovyProgram.groovy
@@ -0,0 +1,7 @@
+package com.samuelvazquez.helloword
+
+class MyFirstGroovyProgram {
+    static void main(String[] args) {
+        println 'Hello World!'
+    }
+}
---
+++ HelloWorld/src/com/samuelvazquez/helloword/hello.groovy
@@ -0,0 +1,5 @@
+package com.samuelvazquez.helloword
+
+def a = 'beautiful'
+println "Hello, $a World"
+println "Hello, World!"
