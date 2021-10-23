7002eda82577e2b69f1283b1be863d0d80faabda
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-10-23 08:16:56 -0500

    added common data types for Groovy

---
+++ HelloWorld/src/com/samuelvazquez/datatypes/DataTypes.groovy
@@ -0,0 +1,40 @@
+package com.samuelvazquez.datatypes
+
+// byte
+byte b = 10
+println b
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+println '=============='
+//short
+short s = 100
+println s
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+println '=============='
+int i = 1000
+println i
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+println '=============='
+int l = 100000
+println l
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+println '=============='
+float f = 100.002F
+println f
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+println '=============='
+double d = 100.0002F
+println d
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+println '=============='
+char c = 'A'
+println c
+boolean flag = true
+println flag
+String str = "Groovy is cool"
+println str
